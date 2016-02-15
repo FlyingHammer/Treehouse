a3c78c000d47e963d7a264692c023f5c4267f928
gmckay <awakenedlife@gmail.com>
2016-02-15 15:57:57 -0500

    1. New Hangman Game

--- src/com/company/Prompter.java
+++ src/com/company/Prompter.java
@@ -3,6 +3,7 @@ package com.company;
 import com.company.Game;
 
 import java.io.Console;
+import java.util.Scanner;
 
 /**
  * Created by gmckay on 2/15/2016.
@@ -16,8 +17,10 @@ public class Prompter {
     }
 
     public boolean PromptForGuess() {
-        Console console = System.console();
-        String guessAsString = console.readLine("Enter a Letter: ");
+        Scanner console = new Scanner(System.in);
+        //String guessAsString = console.readLine("Enter a Letter: ");
+        System.out.printf("Enter a Letter ");
+        String guessAsString = console.nextLine();
         char guess = guessAsString.charAt(0);
         return mGame.applyGuess(guess);
     }
