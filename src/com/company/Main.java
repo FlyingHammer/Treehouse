587958f88eac3403f1b2ef324f40b6535878c680
Gary <awakenedlife@gmail.com>
2016-04-09 13:01:18 -0400

    1. Added WorkSpace and Configuration Files

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,8 @@
 package com.company;
-// Main
+
 public class Main {
 
     public static void main(String[] args) {
-        Game game = new Game("treehouse");
-        Prompter prompter = new Prompter(game);
-        prompter.play();
+	// write your code here
     }
 }
